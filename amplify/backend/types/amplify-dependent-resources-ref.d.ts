1959879da0d8b19477f1d936c9229a421e9a911f
Bijay Kumar Pun <bijay.workplace@gmail.com>
2021-12-09 13:15:25 +0545

    deploy ?

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "testawsauth1c3c9a4c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
